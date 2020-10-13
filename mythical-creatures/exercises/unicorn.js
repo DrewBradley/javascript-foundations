39a9540a70f04aa9a16f206b701bc955a8fe11cd
Drew Bradley <williamandrewb@gmail.com>
2020-10-13 08:40:29 -0600

    Update unicorn.js

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -2,7 +2,23 @@ class Unicorn {
   constructor(name, color) {
     this.name = name;
     this.color = color;
+    
+    if (this.color == undefined){
+      this.color = 'white';
+    }else{
+      this.color == this.color;
+    };
   }
+
+  isWhite(){
+    if (this.color !== 'white') {
+      return false
+    }
+  };
+
+  says(words){
+      return `**;* ${words} *;**`
+  };
 }
 
 module.exports = Unicorn;
